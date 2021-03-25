b8f7749547692d1366868e54b303bcd8ea0f3ce3
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2021-03-24 23:24:11 -0400

    Re-write (optimized) with new README.md

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 ﻿# Update-MofoMojoConfigFile
 
+1. Put this script in a directory
+2. Dot Source the script in to your current PowerShell session . .\Update-MofoMojoConfigFile.ps1
+3. Run one of the examples below to get started!
+
 EXAMPLE
-> Update-MofoMojoMod -Enabled -SettingsToUpdate DisableImACheater
+> Update-MofoMojoConfigFile -SettingsToUpdate DisableImACheater -Enable or - Disable
 
 In this example you can select multiple options by hitting CTRL + Spacebar or typing them out and pass them in as a collection to set each one
+
 EXAMPLE
-> Update-MofoMojoMod -Enabled -SettingsToUpdate DisableImACheater EnableFishingRodRecipe
+> Update-MofoMojoConfigFile -SettingsToUpdate DisableImACheater EnableFishingRodRecipe -Enable or - Disable
 
 Single update example
-> Update-MofoMojoMod -Disabled -SettingsToUpdate DisableImACheater
+> Update-MofoMojoConfigFile -SettingsToUpdate DisableImACheater -Enable or - Disable
 
 Multiple update example
-> Update-MofoMojoMod Disabled -SettingsToUpdate DisableImACheater EnableFishingRodRecipe
+> Update-MofoMojoConfigFile -SettingsToUpdate DisableImACheater, EnableFishingRodRecipe -Enable or - Disable
+
+Enjoy!
--- Update-MofoMojoConfigFile.ps1
+++ Update-MofoMojoConfigFile.ps1
@@ -29,15 +29,15 @@ function Update-MofoMojoConfigFile {
     param (
         
         [Parameter(Position = 0)]
-        [ValidateSet('DisableImACheater', 'EnableFishingRodRecipe', 'EnableLoxMeatSurprise', 'EnableChainsRecipe', 'EnableGuardStoneTweak', 'EnableFeatherMultiplier', 'RemeberLastConnectedIpEnabled')]
+        [ValidateSet('DisableImACheater', 'EnableFishingRodRecipe', 'EnableBaitRecipe', 'EnableLoxMeatSurprise', 'EnableChainsRecipe', 'EnableBronzeTweak', 'EnableGuardStoneTweak', 'EnableFeatherMultiplier', 'EnableFishingInOceanMultiplier', 'RemeberLastConnectedIpEnabled')]
         [object[]]
         $SettingstoUpdate,
 
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, ParameterSetName = 'Flag')]
         [switch]
         $Enable,
 
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, ParameterSetName = 'Flag')]
         [switch]
         $Disable
     )
@@ -49,81 +49,92 @@ function Update-MofoMojoConfigFile {
     }
     
     process {
+        $parameters = $PSBoundParameters
+
         if ($SettingsToUpdate) {
-            $newCfgFile = ""
-            if ( Test-Path -Path (Join-Path -Path $PathToConfigFile -ChildPath $ConfigFileName) ) {
-                $oldCfgFile = Get-Content -Path (join-path -Path $PathToConfigFile -ChildPath $ConfigFileName)
-
-                # Save original configuration file first before making changes
-                $index = (Get-ChildItem -Path $PathToConfigFile -Filter MofoMojo.cfg.bak*).count
-                Rename-Item -Path (join-path -Path $PathToConfigFile -ChildPath $ConfigFileName) -NewName "MofoMojo.cfg.bak($index)" -force
-            }
-            else {
-                Write-Host "$ConfigFileName not found! Exiting."
-                return
-            }
-
-            foreach ($setting in $SettingsToUpdate) {
-                
-                switch ($setting) {
-                    "DisableImACheater" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('DisableImACheater = false', 'DisableImACheater = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('DisableImACheater = true', 'DisableImACheater = false') }
-                        break
-                    } 
-    
-                    "EnableFishingRodRecipe" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('EnableFishingRodRecipe = false', 'EnableFishingRodRecipe = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('EnableFishingRodRecipe = true', 'EnableFishingRodRecipe = false') }
-                        break
-                    }
-    
-                    "EnableLoxMeatSurprise" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('EnableLoxMeatSurprise = false', 'EnableLoxMeatSurprise = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('EnableLoxMeatSurprise = true', 'EnableLoxMeatSurprise = false') }
-                        break
-                    }
-    
-                    "EnableChainsRecipe" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('EnableChainsRecipe = false', 'EnableChainsRecipe = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('EnableChainsRecipe = true', 'EnableChainsRecipe = false') }
-                        break
-                    }
-    
-                    "EnableGuardStoneTweak" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('EnableGuardStoneTweak = false', 'EnableGuardStoneTweak = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('EnableGuardStoneTweak = true', 'EnableGuardStoneTweak = false') }
-                        break
-                    }
-    
-                    "EnableFeatherMultiplier" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('EnableFeatherMultiplier = false', 'EnableFeatherMultiplier = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('EnableFeatherMultiplier = true', 'EnableFeatherMultiplier = false') }
-                        break
-                    }
-    
-                    "RemeberLastConnectedIpEnabled" {
-                        if ($Enable) { $newCfgFile = $oldCfgFile.Replace('RemeberLastConnectedIpEnabled = false', 'RemeberLastConnectedIpEnabled = true') }
-                        if ($Disable) { $newCfgFile = $oldCfgFile.Replace('RemeberLastConnectedIpEnabled = true', 'RemeberLastConnectedIpEnabled = false') }
-                        break
-                    }
-                        
-                    default { Write-Host "No values changed." }
+            $oldCfgFile = @()
+            try {
+					
+                if ( Test-Path -Path (Join-Path -Path $PathToConfigFile -ChildPath $ConfigFileName -ErrorAction Stop) ) {
+                    $script:oldCfgFile = Get-Content -Path (join-path -Path $PathToConfigFile -ChildPath $ConfigFileName) -Raw                    
+                    # Save original configuration file first before making changes
+                    $index = (Get-ChildItem -Path $PathToConfigFile -Filter MofoMojo.cfg.bak* -ErrorAction Stop).count
+                    Rename-Item -Path (Join-Path -Path $PathToConfigFile -ChildPath $ConfigFileName) -NewName "MofoMojo.cfg.bak($index)" -force -ErrorAction Stop
+                }
+                else {
+                    Write-Host "$ConfigFileName not found! Exiting."
+                    return
                 }
             }
-            
+            catch {
+                Write-Host -ForegroundColor Red "ERROR: " $($error[0].Exception.Message)
+            }
 
-            Set-Content -Path ($PathToConfigFile + $ConfigFileName) -Value $newCfgFile -Encoding UTF8 -Force
-            Write-Host "Config file: $($ConfigFileName) updated!"
+            Set-ConfigurationSetting @parameters
+            
+            try {
+                Set-Content -Path ($PathToConfigFile + $ConfigFileName) -Value $script:oldCfgFile -Encoding UTF8 -Force
+                Write-Host "Config file: $($ConfigFileName) updated!"
+            }
+            catch {
+                Write-Host -ForegroundColor Red "ERROR: " $($error[0].Exception.Message)
+            }
         }
         else {
-            # We are just viewing the configuration settings only
             Write-Host "Grabbing config file: $($ConfigFileName) - read only."
-            Get-Content -Path ($PathToConfigFile + $ConfigFileName) 
+            # We are just viewing the configuration settings only
+            
+            try {
+                Get-Content -Path ($PathToConfigFile + $ConfigFileName) -ErrorAction Stop
+            }
+            catch {
+                Write-Host -ForegroundColor Red "ERROR: " $($error[0].Exception.Message)
+            }
         }
     }
     
     end {
         Write-Host "Finished."
+    }
+}
+
+function Set-ConfigurationSetting {
+    <#
+    .SYNOPSIS
+        Enables or disables a setting
+    
+    .DESCRIPTION
+        This function will loop through all of the settings a user wants to change and enable or disable them
+    
+    .PARAMETER SettingstoUpdate
+        The setting(s) to be enabled or disabled
+    
+    .PARAMETER Enable
+        Enable flag
+    
+    .PARAMETER Disable
+        Disable flag
+    
+    .NOTES
+        This is the main helper function
+    #>
+
+    [CmdletBinding()]
+    param (
+        [object[]]
+        $SettingstoUpdate,
+
+        [switch]
+        $Enable,
+        
+        [switch]
+        $Disable
+    )
+
+    process {
+        foreach ($value in $SettingsToUpdate) {
+            if ($Enable) { $script:oldCfgFile = $script:oldCfgFile.Replace("$value = false", "$value = true") }
+            if ($Disable) { $script:oldCfgFile = $script:oldCfgFile.Replace("$value = true", "$value = false") }   
+        }
     }
 }
